0c05fc4f979b3e8ecb00c9b9a3f874b3a2d8396f
manikkang <manikkang@poplify.com>
2016-04-14 20:14:00 +0530

    First commit

---
+++ core/app/models/concerns/spree/default_price.rb
@@ -0,0 +1,26 @@
+module Spree
+  module DefaultPrice
+    extend ActiveSupport::Concern
+
+    included do
+      has_one :default_price,
+        -> { with_deleted.where(currency: Spree::Config[:currency], is_default: true) },
+        class_name: 'Spree::Price',
+        inverse_of: :variant,
+        dependent: :destroy,
+        autosave: true
+    end
+
+    def find_or_build_default_price
+      default_price || build_default_price
+    end
+
+    delegate :display_price, :display_amount, :price, :currency, to: :find_or_build_default_price
+    delegate :price=, :currency=, to: :find_or_build_default_price
+    deprecate :currency=, :currency, deprecator: Spree::Deprecation
+
+    def has_default_price?
+      !default_price.nil?
+    end
+  end
+end
---
+++ core/app/models/spree/price.rb
@@ -0,0 +1,49 @@
+module Spree
+  class Price < Spree::Base
+    acts_as_paranoid
+
+    MAXIMUM_AMOUNT = BigDecimal('99_999_999.99')
+
+    belongs_to :variant, -> { with_deleted }, class_name: 'Spree::Variant', touch: true
+
+    validate :check_price
+    validates :amount, allow_nil: true, numericality: {
+      greater_than_or_equal_to: 0,
+      less_than_or_equal_to: MAXIMUM_AMOUNT
+    }
+
+    after_save :set_default_price
+
+    extend DisplayMoney
+    money_methods :amount, :price
+    alias_method :money, :display_amount
+
+    self.whitelisted_ransackable_attributes = ['amount']
+
+    # An alias for #amount
+    def price
+      amount
+    end
+
+    # Sets this price's amount to a new value, parsing it if the new value is
+    # a string.
+    #
+    # @param price [String, #to_d] a new amount
+    def price=(price)
+      self[:amount] = Spree::LocalizedNumber.parse(price)
+    end
+
+    private
+
+    def check_price
+      self.currency ||= Spree::Config[:currency]
+    end
+
+    def set_default_price
+      if is_default?
+        other_default_prices = variant.prices.where(currency: self.currency, is_default: true).where.not(id: id)
+        other_default_prices.update_all(is_default: false)
+      end
+    end
+  end
+end
